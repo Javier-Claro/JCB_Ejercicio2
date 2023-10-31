f1ca845ae5a5d1ed6b5542f3f46881c5f0fe3a03
DESKTOP-D5NS5P4\manuel <manuelclarobarba@gmail.com>
2023-10-31 23:03:36 +0100

    Agregar archivos de proyecto.

---
+++ Ejercicio2/Servicios/PreguntaImplementacion.cs
@@ -0,0 +1,20 @@
+﻿using Ejercicio2.Servicios;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ejercicio2NumernoElevado.Servicios
+{
+    internal class PreguntaImplemetacion : PreguntaInterfaz
+    {
+        public double pedirNumero()
+        {
+            Console.Write("Escriba un numero ");
+            double numeroPedido = Convert.ToInt32(Console.ReadLine());
+            return numeroPedido;
+        }
+
+    }
+}
